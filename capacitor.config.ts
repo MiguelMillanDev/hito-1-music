9aa4cd20ff076999f082693d806832f74fbcad3c
TrevorDeey <mmillan250@alumno.uned.es>
2024-01-28 19:27:21 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Hito-1-Music',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
